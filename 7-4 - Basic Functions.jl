549521cf6350e9547cff93a71b58987dc3fe5d1d
Aditya <tadityareddy20122002@gmail.com>
2023-11-06 08:28:43 +0530

    Delete 7-4 - Basic Functions.jl

--- 7-4 - Basic Functions.jl
+++
@@ -1,9 +0,0 @@
-# Count occurrences of elements in an array.
-
-function counter(array_of_elements)
-    counts = Dict()
-    for element in array_of_elements
-        counts[element] = get(counts, element, 0) + 1
-    end
-    counts
-end
